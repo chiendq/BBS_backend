8117d9f3f27b33a9bdb132686cbddc3562e5f91b
chien_dq <chien_dq@flinters.vn>
2022-07-18 10:53:00 +0700

    change database and update evolutions script

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,14 +2,26 @@
 
 -- !Ups
 
-CREATE TABLE user (
-                      id bigint(20) NOT NULL AUTO_INCREMENT,
-                      email varchar(255) NOT NULL,
-                      password varchar(255) NOT NULL,
-                      fullname varchar(255) NOT NULL,
-                      isAdmin boolean NOT NULL,
-                      PRIMARY KEY (id)
+CREATE TABLE `post` (
+                        `id` bigint PRIMARY KEY AUTO_INCREMENT,
+                        `title` nvarchar(150) NOT NULL,
+                        `content` text NOT NULL,
+                        `author_name` nvarchar(50) NOT NULL,
+                        `created_at` datetime NOT NULL,
+                        `updated_on` datetime NOT NULL,
+                        `thumbnail` text NOT NULL ,
+                        `account_id` bigint NOT NULL
 );
+
+CREATE TABLE `account` (
+                           `id` bigint PRIMARY KEY AUTO_INCREMENT,
+                           `username` varchar(50) NOT NULL,
+                           `email` varchar(255) NOT NULL,
+                           `password` varchar(255) NOT NULL
+);
+
+ALTER TABLE `post` ADD FOREIGN KEY (`account_id`) REFERENCES `account` (`id`);
+
 
 -- !Downs
 
