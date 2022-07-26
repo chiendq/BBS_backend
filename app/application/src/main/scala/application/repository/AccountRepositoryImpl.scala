9920ffa6f3255200f01c67b75c4a5226f7fb4d2e
chien_dq <chien_dq@flinters.vn>
2022-07-26 17:53:14 +0700

    create an easter egg

--- app/application/src/main/scala/application/repository/AccountRepositoryImpl.scala
+++ app/application/src/main/scala/application/repository/AccountRepositoryImpl.scala
@@ -5,21 +5,27 @@ import domain.account.model.{Account, AccountId}
 import infrastructure.mySqlDao.AccountDao
 
 import java.util.UUID
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 /**
  * TODO: Not yet validate
  */
 class AccountRepositoryImpl extends AccountRepository{
   override def save(account: Account): Try[AccountId] = {
-//    if (isExistEmail(account.email)) throw new RuntimeException("Email already exist")
-    Try{
+    if (isExistEmail(account.email)) throw new RuntimeException("Email already exist")
+    Try {
       AccountDao.createWithAttributes(
         Symbol("id") -> account.id.value,
         Symbol("username") -> account.username,
         Symbol("email") -> account.email,
         Symbol("password") -> account.password,
       )
+    } match {
+      /**
+       * Easter Egg
+       */
+      case Failure(exception) => Try{AccountId("")}
+      case Success(value) => Try{value}
     }
   }
 
