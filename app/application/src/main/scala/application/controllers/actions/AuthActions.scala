0cf562353e8b905e2f09b97769240b19e26c25c3
chien_dq <chien_dq@flinters.vn>
2022-08-01 15:31:14 +0700

    shortened logic code

--- app/application/src/main/scala/application/controllers/actions/AuthActions.scala
+++ app/application/src/main/scala/application/controllers/actions/AuthActions.scala
@@ -1,39 +1,32 @@
 package application.controllers.actions
 
-import application.controllers.payload.UserRequest
-import play.api.mvc._
 import application.services.AuthServiceImpl
 import play.api.Logger
+import play.api.mvc._
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
 class AuthActions @Inject()(bodyParser: BodyParsers.Default, authService: AuthServiceImpl)(implicit ec: ExecutionContext)
-  extends ActionBuilder[UserRequest, AnyContent] {
+  extends ActionBuilder[Request, AnyContent] {
+
+  lazy val logger: Logger = Logger(getClass)
+  override protected def executionContext: ExecutionContext = ec
 
   override def parser: BodyParser[AnyContent] = bodyParser
 
-  override protected def executionContext: ExecutionContext = ec
-
-  lazy val logger: Logger = Logger(getClass)
-
-  override def invokeBlock[A](request: Request[A], block: UserRequest[A] => Future[Result]): Future[Result] = {
+  override def invokeBlock[A](request: Request[A], block: Request[A] => Future[Result]): Future[Result] = {
     Try {
-      authService.extractBearerToken(request)
+      authService.extractBearerToken(request).get
     } match {
+      case Success(value) =>
+        authService.validateJwt(value) match {
+          case Success(claim) => block(request)
+          case Failure(t) => Future.successful(Results.Unauthorized(t.getMessage))
+        }
       case Failure(exception) => Future.successful(Results.Unauthorized(exception.getMessage))
-      case Success(value) => value match {
-        case Some(token) =>
-          authService.validateJwt(token) match {
-            case Success(claim) => block(UserRequest(request)) // token was valid - proceed!
-            case Failure(t) => Future.successful(Results.Unauthorized(t.getMessage)) // token was invalid - return 401
-          }
-        case _ => Future.successful(Results.Unauthorized)
-    }
     }
   }
-
-
 
 }
